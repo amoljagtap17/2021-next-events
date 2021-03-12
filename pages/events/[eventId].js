3fbef4cd27d6d8db6d32c1e11396ff8d918ee407
Amol Jagtap <ajfsdeveloper@gmail.com>
2021-03-12 20:01:53 +0530

    NEXT_EVENTS: Define Main pages

---
+++ pages/events/[eventId].js
@@ -0,0 +1,5 @@
+function EventDetailPage() {
+  return <h1 className="center">Event Detail Page...</h1>
+}
+
+export default EventDetailPage
---
+++ pages/events/index.js
@@ -0,0 +1,5 @@
+function EventsPage() {
+  return <h1 className="center">Events Page...</h1>
+}
+
+export default EventsPage
