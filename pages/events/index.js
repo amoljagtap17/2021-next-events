a2036ede9c5f744fd97aa21a4dacd7f2de4f5271
Amol Jagtap <ajfsdeveloper@gmail.com>
2021-03-13 14:25:22 +0530

    NEXT_EVENTS: Navigate to FilteredEvents page

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,12 +1,20 @@
+import { useRouter } from 'next/router'
 import { EventList, EventsSearch } from 'components/events'
 import { getAllEvents } from 'dummy-data'
 
 function EventsPage() {
+  const router = useRouter()
   const events = getAllEvents()
+
+  const findEventsHandler = (year, month) => {
+    const path = `/events/${year}/${month}`
+
+    router.push(path)
+  }
 
   return (
     <>
-      <EventsSearch />
+      <EventsSearch onSearch={findEventsHandler} />
       <EventList events={events} />
     </>
   )
