324f1e9b281f70a47ec6a7ba7537959e5c9009b7
Amol Jagtap <ajfsdeveloper@gmail.com>
2021-03-18 20:09:58 +0530

    NEXT_EVENTS: Using Server side rendering

--- pages/events/[...slug].js
+++ pages/events/[...slug].js
@@ -1,17 +1,72 @@
 import { useRouter } from 'next/router'
-import { getFilteredEvents } from 'dummy-data'
+import { getFilteredEvents } from 'helpers/api-util'
 import { EventList, ResultsTitle } from 'components/events'
 import { Button, ErrorAlert } from 'components/ui'
 
-function FilteredEventsPage() {
-  const {
+function FilteredEventsPage({ hasError, events, date: { year, month } }) {
+  /* const {
     query: { slug },
-  } = useRouter()
+  } = useRouter() */
 
   // useRouter is executed after component is rendered
-  if (!slug) {
+  /* if (!slug) {
     return <p className="center">Loading...</p>
   }
+
+  const filteredYear = slug[0]
+  const filteredMonth = slug[1]
+
+  const numYear = +filteredYear
+  const numMonth = +filteredMonth */
+
+  if (hasError) {
+    return (
+      <>
+        <ErrorAlert>
+          <p>Invalid filter..Adjust your values</p>
+        </ErrorAlert>
+        <div className="center">
+          <Button link="/events">Show All Events</Button>
+        </div>
+      </>
+    )
+  }
+
+  /* const filteredEvents = getFilteredEvents({
+    year: numYear,
+    month: numMonth,
+  }) */
+
+  const filteredEvents = events
+
+  if (!filteredEvents || filteredEvents.length === 0) {
+    return (
+      <>
+        <ErrorAlert>
+          <p>No events found for the selected filters...</p>
+        </ErrorAlert>
+        <div className="center">
+          <Button link="/events">Show All Events</Button>
+        </div>
+      </>
+    )
+  }
+
+  // const date = new Date(numYear, numMonth - 1)
+  const date = new Date(year, month - 1)
+
+  return (
+    <>
+      <ResultsTitle date={date} />
+      <EventList events={filteredEvents} />
+    </>
+  )
+}
+
+export async function getServerSideProps(context) {
+  const {
+    params: { slug },
+  } = context
 
   const filteredYear = slug[0]
   const filteredMonth = slug[1]
@@ -27,44 +82,32 @@ function FilteredEventsPage() {
     numMonth < 1 ||
     numMonth > 12
   ) {
-    return (
-      <>
-        <ErrorAlert>
-          <p>Invalid filter..Adjust your values</p>
-        </ErrorAlert>
-        <div className="center">
-          <Button link="/events">Show All Events</Button>
-        </div>
-      </>
-    )
+    return {
+      props: {
+        hasError: true,
+      },
+      // notFound: true,
+      // We could redirect instead of showing not found page. But since we are not having error page we are ignoring
+      // redirect: {
+      //   destination: '/error-page'
+      // }
+    }
   }
 
-  const filteredEvents = getFilteredEvents({
+  const events = await getFilteredEvents({
     year: numYear,
     month: numMonth,
   })
 
-  if (!filteredEvents || filteredEvents.length === 0) {
-    return (
-      <>
-        <ErrorAlert>
-          <p>No events found for the selected filters...</p>
-        </ErrorAlert>
-        <div className="center">
-          <Button link="/events">Show All Events</Button>
-        </div>
-      </>
-    )
+  return {
+    props: {
+      events,
+      date: {
+        year: numYear,
+        month: numMonth,
+      },
+    },
   }
-
-  const date = new Date(numYear, numMonth - 1)
-
-  return (
-    <>
-      <ResultsTitle date={date} />
-      <EventList events={filteredEvents} />
-    </>
-  )
 }
 
 export default FilteredEventsPage
