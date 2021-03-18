268460b38b39c83c1aa09995b54975aa781f608a
Amol Jagtap <ajfsdeveloper@gmail.com>
2021-03-18 20:42:08 +0530

    NEXT_EVENTS: Configuring the Head

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,18 @@
+import Head from 'next/head'
 import { getFeaturedEvents } from 'helpers/api-util'
 import { EventList } from 'components/events'
 
 function HomePage({ featuredEvents }) {
+  // Head can be used anywhere inside JSX
   return (
     <div>
+      <Head>
+        <title>NextJS Events</title>
+        <meta
+          name="description"
+          content="Find a lot of events that allow you to evolve..."
+        />
+      </Head>
       <EventList events={featuredEvents} />
     </div>
   )
