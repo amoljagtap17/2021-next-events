e596db4646e32ea01c681584336aacc3e351683e
Amol Jagtap <ajfsdeveloper@gmail.com>
2021-03-18 21:09:34 +0530

    NEXT_EVENTS: Merging Head content

--- pages/_app.js
+++ pages/_app.js
@@ -3,9 +3,13 @@ import { Layout } from 'components/layout'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
+  // multiple Head sections are merged.
+  // Also latest values are used from multiple Head sections
   return (
     <Layout>
       <Head>
+        <title>Next Events</title>
+        <meta description="" content="NextJS Events" />
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
       <Component {...pageProps} />
